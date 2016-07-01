a194ab7ee623744aeffba79acae16ed93397aaa0
xiaopeng <18971903450@163.com>
2016-07-01 16:31:54 +0800

    修复游戏币修改功能

--- src/com/company/UI/UImonster.java
+++ src/com/company/UI/UImonster.java
@@ -21,16 +21,18 @@ public class UImonster {
         timer.schedule(new TimerTask() {
             int a=0;
             public void run(){
+               int luck= (int) (Math.random() * 100);
                 a++;
                 if (a<25) {
                     System.out.print("*");
                 }
                 if(a==25){
-                    System.out.println("\n````````````````````````````````````````````````````````````````````````````````````````\n当前共打了"+a+"只小怪兽\n共获得"+a*10+"点经验\n获得"+a*3+"文钱\n````````````````````````````````````````````````````````````````````````````````````````");
+                    System.out.println("\n````````````````````````````````````````````````````````````````````````````````````````\n当前共打了"+luck+"只小怪兽\n共获得"+luck*10+"点经验\n获得"+luck*3+"文钱\n````````````````````````````````````````````````````````````````````````````````````````");
                     timer.cancel();
-                    int [] info=userwork.UserworkGrade(ds.getInteger("exp")+(a*10),ds.getInteger("grade"));
+                    int [] info=userwork.UserworkGrade(ds.getInteger("exp")+(luck*10),ds.getInteger("grade"));
                     ds.put("grade",info[0]);
                     ds.put("exp",info[1]);
+                    ds.put("gamemoney",ds.getInteger("gamemoney")+luck*3);
                     mogo.UpdateUser(ds);
                     uiGameFunction.UIGameFunctionmain(mogo,ds);
                 }
